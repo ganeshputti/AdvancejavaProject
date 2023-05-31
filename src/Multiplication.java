e2bedb8d7f32cdf45cb429b475a8d8a50115b2cd
RaviTeja2K <raviteja.e25@gmail.com>
2023-05-31 18:21:22 +0530

    modified

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -2,9 +2,6 @@
 public class Multiplication {
 
 	public static void main(String[]args) {
-		int z = 30;
-		int k = 54;
-		int y = z * k;
-		System.out.println(y);
+		System.out.println("hello world.......!");
 	}
 }
